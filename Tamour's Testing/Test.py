01029eda25bdf997b60932c67c0da7242936b6b8
RampageTheBandit <tamouraltaf99@gmail.com>
2020-08-06 15:56:26 +0100

    ds

--- Tamour's Testing/Test.py
+++ Tamour's Testing/Test.py
@@ -2,9 +2,9 @@ import collections
 import keras
 
 import numpy as np
-import tensorflow as tf
 np.random.seed(0)
 
 import tensorflow_federated as tff
+
 
 print(tff.federated_computation(lambda: 'Hello World')())
