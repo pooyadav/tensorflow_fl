121547178f8899086bc678e1f23acf580915f135
RampageTheBandit <tamouraltaf99@gmail.com>
2020-08-07 15:14:47 +0100

    Update to repo of work so far

---
+++ Tamour's Testing/Basic image classifcation.py
@@ -0,0 +1,21 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+#importing fashion dataset
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
